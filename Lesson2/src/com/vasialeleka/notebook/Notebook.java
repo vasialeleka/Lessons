80dfef68053ef00b6af1ee3d538304a4c7823f60
Vasia <vasialeleka@ukr.net>
2018-08-21 22:36:09 +0300

    Swich changed on if (Notebook)

--- Lesson2/src/com/vasialeleka/notebook/Notebook.java
+++ Lesson2/src/com/vasialeleka/notebook/Notebook.java
@@ -6,45 +6,27 @@ import java.util.Scanner;
 
 public class Notebook {
     public static void main(String[] args) {
-        boolean flag = true;
-        //int i = 0;
-       List<String> notes = new ArrayList<>();
-        while (flag) {
+        List<String> notes = new ArrayList<>();
+        while (true) {
             System.out.println("Push 1 for new note.\n 2 - see all notes.\n 3- close");
             Scanner in = new Scanner(System.in);
             int n = in.nextInt();
 
-            switch (n) {
-                case 1:
-                    System.out.println("Write your notes :");
-                    Scanner noteLine = new Scanner(System.in);
-                    String note;
-                    note = noteLine.nextLine();
-                    notes.add(note);
-                    //System.out.flush();
-                    //System.out.println(note);
-                    break;
-                case 2:
-                    for (int i= 0; i <notes.size(); i++){
-
-                    System.out.println("Note "+(i+1)+":"+notes.get(i));
-                        System.out.println("");}
-                   // System.out.println(Notes.size());
-                    break;
-                case 3:
-                    flag = false;
-                    break;
-
-                default:
-                    System.out.println("I don't know");
-
-                     break;
-
+            if (n == 1) {
+                System.out.println("Write your notes :");
+                Scanner noteLine = new Scanner(System.in);
+                String note;
+                note = noteLine.nextLine();
+                notes.add(note);
+            } else if (n == 2) {
+                for (int i = 0; i < notes.size(); i++) {
+                    System.out.println("Note " + (i + 1) + ":" + notes.get(i));
+                    System.out.println();
+                }
+            } else {
+                break;
             }
-
-
-
-
         }
     }
 }
+
