b46663148d248d7096785113821111f4a65a9d12
Vasia <vasialeleka@ukr.net>
2018-08-24 21:47:10 +0300

    New type of changes (Notebook)

--- Lesson2/src/com/vasialeleka/notebook/Notebook.java
+++ Lesson2/src/com/vasialeleka/notebook/Notebook.java
@@ -17,33 +17,12 @@ public class Notebook {
             int n = in.nextInt();
 
             if (n == 1) {
-                System.out.println("Write your notes :");
-                Scanner noteLine = new Scanner(System.in);
-                String note;
-                note = noteLine.nextLine();
-                notes[countOfNotes] = note;
-                countOfNotes++;
+                countOfNotes = WriteOfNotes(notes, countOfNotes);
             } else if (n == 2) {
-                for (int i = 0; i < countOfNotes; i++) {
-                    System.out.println("Note " + (i + 1) + ":" + notes[i]);
-                    System.out.println();
-                }
+                ShowNotes(notes, countOfNotes);
             } else if (n == 3) {
-                for (int i = 0; i < countOfNotes; i++) {
-                    System.out.println("Note " + (i + 1) + ":" + notes[i]);
-                    System.out.println();
-                }
-                System.out.println("Choose what note you want to delete :");
-                Scanner delete = new Scanner(System.in);
-                int del = delete.nextInt();
-                for (int i = 0; i < countOfNotes; i++) {
-                    if (del == (i + 1)) {
-                        for (int j = del; j <= countOfNotes; j++) {
-                            notes[j - 1] = notes[j];
-                        }
-                        countOfNotes--;
-                    }
-                }
+                ShowNotes(notes, countOfNotes);
+                countOfNotes = Delete(notes, countOfNotes);
             } else if (n == 4) {
                 System.out.println("4");
                 for (int i = 0; i < notes.length; i++) {
@@ -59,16 +38,75 @@ public class Notebook {
                 int ch = change.nextInt();
                 for (int i = 0; i < countOfNotes; i++) {
                     if (ch == (i + 1)) {
+                        //  System.out.println(notes[i]);
+
+                        //System.out.println();
+                        //System.out.println();
+
+
+
+                    System.out.println("Push 1 if you want to add words to you note.\n 2 - if you want to create new from begin.\n 3-close");
+                    Scanner changeNote = new Scanner(System.in);
+                    int changedNote = changeNote.nextInt();
+                    if (changedNote == 1) {
+                       // char[] charNote = notes[i].toCharArray();
+
+                        //for (int countOfSymbols = 0; countOfSymbols < charNote.length; countOfSymbols++) {
+                          //  System.out.print(charNote[countOfSymbols]);
+
+                        //}
+                        System.out.print(notes[i]);
+                        Scanner newNote = new Scanner(System.in);
+                        String addToNote = newNote.nextLine();
+                        notes[i] = notes[i] + addToNote;
+                        System.out.println(notes[i]);
+
+                    } else if (changedNote == 2) {
+                        System.out.println();
                         System.out.println("Enter new note:");
                         Scanner newNote = new Scanner(System.in);
                         notes[i] = newNote.nextLine();
-                    }
+                    } else{break;}}
                 }
+
+
             } else {
                 break;
             }
 
         }
+    }
+
+    private static int Delete(String[] notes, int countOfNotes) {
+        System.out.println("Choose what note you want to delete :");
+        Scanner delete = new Scanner(System.in);
+        int del = delete.nextInt();
+        for (int i = 0; i < countOfNotes; i++) {
+            if (del == (i + 1)) {
+                for (int j = del; j <= countOfNotes; j++) {
+                    notes[j - 1] = notes[j];
+                }
+                countOfNotes--;
+            }
+        }
+        return countOfNotes;
+    }
+
+    private static void ShowNotes(String[] notes, int countOfNotes) {
+        for (int i = 0; i < countOfNotes; i++) {
+            System.out.println("Note " + (i + 1) + ":" + notes[i]);
+            System.out.println();
+        }
+    }
+
+    private static int WriteOfNotes(String[] notes, int countOfNotes) {
+        System.out.println("Write your notes :");
+        Scanner noteLine = new Scanner(System.in);
+        String note;
+        note = noteLine.nextLine();
+        notes[countOfNotes] = note;
+        countOfNotes++;
+        return countOfNotes;
     }
 }
 
